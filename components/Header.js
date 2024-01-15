0ed853ad316bb78037725b4d529ffe6507d0d84c
nimdaresu <156701754+nimdaresu@users.noreply.github.com>
2024-01-15 12:05:44 -0800

    Update Header.js
    testing

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,14 @@
 export default function Header({ title }) {
-  return <h1 className="title">{title}</h1>
+  return (
+    <header>
+      <h1 className="title">{title}</h1>
+      <nav>
+        <a href="/">Home</a>
+        <a href="/portfolio">Portfolio</a>
+        <a href="/services">Services</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+      </nav>
+    </header>
+  );
 }
