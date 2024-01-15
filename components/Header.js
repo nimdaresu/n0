b4b16b565d7053baa3b6c89bca860a2a83ceac80
nimdaresu <156701754+nimdaresu@users.noreply.github.com>
2024-01-15 12:12:02 -0800

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,17 @@
 export default function Header({ title }) {
   return (
     <header>
-      <h1 className="title">{title}</h1>
       <nav>
-        <a href="/">Home</a>
-        <a href="/portfolio">Portfolio</a>
-        <a href="/services">Services</a>
-        <a href="/about">About</a>
-        <a href="/contact">Contact</a>
+        {/* List of navigation links */}
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#portfolio">Portfolio</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
       </nav>
+      <h1 className="title">{title}</h1>
     </header>
-  );
+  )
 }
