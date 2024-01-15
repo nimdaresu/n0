24e21b550fba79befeb92e4fa52fbd67505a376d
nimdaresu <156701754+nimdaresu@users.noreply.github.com>
2024-01-15 12:08:57 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,11 +10,26 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <Header title="Get Started" />
+
       <main>
-        <Header title="n0 next n0xt" />
-        <p className="description">
-          paragraph paragram <code>paran0</code>
-        </p>
+        <section className="hero">
+          <blockquote>
+            "Applications starve without data. Applications bloat with bad data. Daily good data"
+          </blockquote>
+          <cite>N0</cite>
+          <img src="/path-to-your-hero-image.jpg" alt="Book cover with design quote" />
+        </section>
+
+        <section className="availability">
+          <h2>n0 amz/h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Temporibus possimus, aut veniam. Ut minus, possimus quisquam
+            cupiditate magnam nisi pariatur...
+          </p>
+          <button className="hire-us">Hire Us Now</button>
+        </section>
       </main>
 
       <Footer />
