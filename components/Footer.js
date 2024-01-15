65af351359670c1bcc9e226bc1900e94a7a223f6
nimdaresu <156701754+nimdaresu@users.noreply.github.com>
2024-01-15 10:07:18 -0800

    Update Footer.js
    replace svg

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
+        <img src="/n0.svg" alt="n0" className={styles.logo} />
       </footer>
     </>
   )
