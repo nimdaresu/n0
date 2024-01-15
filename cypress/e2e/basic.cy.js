caf90375fed943b3c5c5a83b80a1d111c42e82d6
nimdaresu <156701754+nimdaresu@users.noreply.github.com>
2024-01-15 10:14:07 -0800

    Update basic.cy.js

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -1,13 +1,13 @@
-describe('sample test', () => {
+describe('test', () => {
   beforeEach(() => {
     cy.visit('/')
   })
 
   it('displays the resources text', () => {
     cy.get('h1')
-    .contains('Welcome to my app!');
+    .contains('h1 load n0');
   })
-  it('renders the Netlify logo image', () => {
+  it('render logo image', () => {
     cy.get('img')
     .should('be.visible')
     .and(($img) => {
